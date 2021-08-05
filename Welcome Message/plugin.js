8e8346b344986714b2d602ac8c8d244e29dd3709
JuiciiOfficial <56958664+JuiciiYT@users.noreply.github.com>
2021-08-05 13:20:48 +0100

    Delete Welcome Message directory

--- Welcome Message/plugin.js
+++
@@ -1,6 +0,0 @@
-if (document.title == "RBXUtil Menu") {
-  const NOTIFICATION_TITLE = 'Welcome to RBXUtil!'
-  const NOTIFICATION_BODY = 'Time to get searchin\''
-
-  new Notification(NOTIFICATION_TITLE, { body: NOTIFICATION_BODY })
-}
